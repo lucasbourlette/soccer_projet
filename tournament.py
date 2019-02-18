7b3db0e2a6798368bca837c27392f7cb56f075f8
lucasbourlette <44023276+lucasbourlette@users.noreply.github.com>
2019-02-18 16:17:15 +0100

    Add files via upload

--- tournament.py
+++ tournament.py
@@ -6,15 +6,19 @@ Created on Mon Feb  4 16:29:58 2019
 @author: 3672216
 """
 
-from projet import FonceurStrategy, DefenseStrategy
-from soccersimulator import SoccerTeam
+from mbappepresident import FonceurStrategy, DefenseStrategy
+from soccersimulator import Simulation, show_simu
+from mbappepresident import get_team
 
-def get_team(nb_players):
-    team = SoccerTeam(name = "Lucas's Team")
-    if nb_players == 1:
-        team.add("mitroglou", FonceurStrategy())
-    if nb_players == 2:
-        team.add("mitroglou", FonceurStrategy())
-        team.add("Kante", DefenseStrategy())
-    return team
+team1 = get_team(1)
+team2 = get_team(2)
 
+# Add players
+#team1.add("Random", RandomStrategy()) 
+#team2.add("manel", DefenseStrategy())  # Random strategy
+#team1.add("zizou", FonceurStrategy())   # Static strategy
+
+simu = Simulation(team1, team2)
+
+# Simulate and display the match
+show_simu(simu)
