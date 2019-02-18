0c936553bf58d5079a3eb426bbb16a0f214d15b6
lucasbourlette <44023276+lucasbourlette@users.noreply.github.com>
2019-02-18 11:58:51 +0100

    Add files via upload

--- projet.py
+++ projet.py
@@ -52,39 +52,7 @@ class Fonceur2Strategy(Strategy):
     def compute_strategy(self, state, id_team, id_player):
         s = tools.MyState(state,id_team,id_player)
         return s.petit_tire
-"""class joueur_attaquant(Strategy):
-     def __init__(self):
-        Strategy.__init__(self, "Attaquant")
-        
-     def compute_strategy(self, state, id_team, id_player):
-        s= tools.MyState(state,id_team,id_player)
-        i=s.goal.x
-        if id_team==2: 
-           if  s.ball_positionx > settings.GAME_WIDTH/100*70:  
-                return SoccerAction((Vector2D(settings.GAME_WIDTH/100*65,settings.GAME_HEIGHT/2)-s.my_position),Vector2D())
-        if id_team==1:
-            if s.ball_positionx < settings.GAME_WIDTH/100*30:
-                return SoccerAction((Vector2D(settings.GAME_WIDTH/100*35,settings.GAME_HEIGHT/2)-s.my_position),Vector2D())
-        if s.norme>settings.SHORT_RANGE:
-           return SoccerAction((s.ball_position + s.ball_vitesse*10-s.my_position).normalize()*settings.maxPlayerAcceleration,Vector2D())
-        if s.norme>settings.BALL_RADIUS:
-            return SoccerAction((s.ball_position-s.my_position),Vector2D())
-        if (s.my_position.x<settings.GAME_WIDTH/2 and s.id_team==1)or(s.my_position.x>settings.GAME_WIDTH/2 and s.id_team==2):
-           if s.eproche<settings.SHORT_RANGE:
-                return SoccerAction((s.ball-s.player),(s.fproche).normalize()*2)
-           return SoccerAction((s.ball-s.player),(Vector2D(i,settings.GAME_HEIGHT/2)-s.player).normalize()*2)
-        else:
-           return SoccerAction((s.ball-s.player),Vector2D(i,settings.GAME_HEIGHT/2)-s.player)"""
-    
-       
-        
-"""class AttStrate(Strategy):
-    def __init__(self):
-        Strategy.__init__(self, "Defense")
 
-    def compute_strategy(self, state, id_team, id_player):
-         s = tools.MyState(state,id_team,id_player)
-       return SoccerAction(Vector2D(GAME_WIDTH*(state.teamdef[0]), (s.balle_futur.y+s.goal.y)/2 )-state.player, state.goal-state.player)"""
    
 team1 = SoccerTeam(name="Team 1")
 team2 = SoccerTeam(name="Team 2")
